57c0192f7f04220547dbf4052d6fe6a050dfdbb2
Masoud Shahpoori <masoudshahpoori97@gmail.com>
2021-08-14 10:55:05 +0430

    got ready for presenting

--- src/App.js
+++ src/App.js
@@ -1,49 +1,4 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import {connect} from 'react-redux'
-// import {bindActionCreators} from 'redux';
-// import React from 'react'
-// import {handleSearch} from './redux/Action/productAction'
-// import ProductList from './product/productList'
-// 
-// 
-// // 
-
-
-
-// function App({handleSearch,result,loading}) {
-
-
-
-//   return (
-//     <div className="App">
-//       <div className="text-center main-section">
-//       <div className="container-search-input">
-//         <p className="text-left">please enter city</p>
-//                 <input type="text" onChange={(e) => handleSearch(e.target.value)} />
-
-//       </div>
-
-//       </div>
-
-//       <ProductList data={result} loading={loading}></ProductList>
-//     </div>
-//   );
-// }
-
-// const mapStateToProps = ({product:{result,loading}})=>({
-//   result:result,
-//   loading:loading
-// })
-// const mapDispatchToProps = dispatch =>{
-
-//   return bindActionCreators({handleSearch}, dispatch);
-
-// }
-
-// export default connect(mapStateToProps,mapDispatchToProps)(App)  ;
-
-// npm i redux react-redux redux-logger redux-thunk axios bootstrap sass react-bootstrap-icons react-router-dom
+// npm i redux react-redux redux-thunk axios bootstrap sass react-bootstrap-icons react-router-dom
 import React from 'react'
 import {
   Switch,
@@ -53,7 +8,7 @@ import {
 import HomePage from './pages/homePage'
 import DayDetail from './pages/dayDetail'
 import ErrorPage from './pages/errorPage'
-import './Style/index.scss'
+import './style/index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function App() {
